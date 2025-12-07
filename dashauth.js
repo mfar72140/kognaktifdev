43e7bddb4bfd1db30b163e5d8fef5274abea15aa
mfar72140 <firdausrazak86@gmail.com>
2025-12-08 01:33:37 +0800

    Update 3

--- dashauth.js
+++ dashauth.js
@@ -1,4 +1,9 @@
-import { supabase } from './supabaseClient.js';
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://ijugaqvsszticpzrznnb.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqdWdhcXZzc3p0aWNwenJ6bm5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MjIwMjksImV4cCI6MjA3MzE5ODAyOX0.kx29Dr1eAIKdeRwpH9VBYerKuHYWz2wIvNYc85ms5mY"
+);
 
 export async function loadUser() {
   const { data: { user } } = await supabase.auth.getUser();
