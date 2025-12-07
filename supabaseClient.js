bc4ceb75768e261e0c395be258083ef2f7569d92
mfar72140 <firdausrazak86@gmail.com>
2025-12-08 00:46:20 +0800

    Update 2

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from  "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from  "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
 
 export const supabase = createClient(
   "https://ijugaqvsszticpzrznnb.supabase.co",
